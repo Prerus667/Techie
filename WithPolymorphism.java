468da4dce6eba209d2e02670bcea1b835577830c
Prerus667 <31545049+Prerus667@users.noreply.github.com>
2017-09-21 00:27:40 +0100

    Add files via upload

---
+++ WithPolymorphism.java
@@ -0,0 +1,31 @@
+interface IDraw{
+	public void draw();
+}
+
+class Circle implements IDraw{
+	public void draw(){
+		System.out.println("Draw a circle");
+	}
+}
+
+class Square implements IDraw{
+	public void draw(){
+		System.out.println("Draw a square");
+
+		}
+	}
+
+ public class WithPolymorphysim{
+	public static void main(String [] args){
+		//List<MyType> myList = new ArrayList<MyType>();
+		List<IDraw> myList = new ArrayList<IDraw>();
+		myList.add(new Circle());
+		myList.add(new Square());
+		
+		for(IDraw item : myList){ //for any iDraw item in myList do item.draw()
+			item.draw();
+		}
+		
+	}
+
+}
---
+++ example1.py
@@ -0,0 +1,7 @@
+
+
+var = 10
+
+if ( var  == 100 ) : print ('Value of expression is 100');
+elif (var !=10): print("value is not 100");
+else: print("value is 10");
